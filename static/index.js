578ce2ea0b6c38f1e5dabef66c625e9c21360561
Matthew Connelly <matthew@harbourshare.com>
2022-02-22 10:57:26 -0500

    buefy layout

--- static/index.js
+++ static/index.js
@@ -2,14 +2,4 @@ const app = new Vue({
     el: '#root',
     data: {
     },
-    template: `
-    <div class="app">
-        <div class="img-ctn">
-            <b-image
-            src="https://picsum.photos/600/400"
-            ratio="6by4"
-            />  
-        </div>
-    </div>
-    `
 })
